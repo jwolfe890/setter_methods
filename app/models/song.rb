8ac94bfade0684ee8f834804551baba02bcb1cf2
jwolfe890 <jwolfe890@gmail.com>
2016-12-31 12:23:12 -0500

    first commit

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,15 @@
 class Song < ActiveRecord::Base
+  
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+
+
+  def artist_name=(name)
+    binding.pry 
+    artist = Artist.find_or_create_by(name: name)
+    self.artist_id = artist.id
+  end 
+
 end
 
